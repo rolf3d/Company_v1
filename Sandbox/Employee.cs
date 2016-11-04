ca11a9369912277945c1c4e9715161c96ce0877e
Martin Kierkegaard <mark@easj.dk>
2016-11-04 12:27:14 +0100

    Add project files.

---
+++ Sandbox/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Employee
+    {
+        private String name;
+        private int salaryPerMonth;
+
+        public Employee(String name, int salaryPerMonth)
+        {
+            this.name = name;
+            this.salaryPerMonth = salaryPerMonth;
+        }
+
+        public String GetName()
+        {
+            return name;
+        }
+
+        public virtual int GetSalaryPerMonth()
+        {
+            return salaryPerMonth;
+        }
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+
+            // Add you test of the Employee class and derived classes here
+
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
